915c4a328abc515e241e0ef20b64241a96ae891c
J.D. Lowe <jdlowe.dev@gmail.com>
2017-03-10 16:01:44 -0500

    More water toeing

--- src/emu/tracker/GameTracker.cpp
+++ src/emu/tracker/GameTracker.cpp
@@ -9,3 +9,13 @@ void GameTracker::clearStats()
 	//	stats.clear();
 	return;
 }
+
+uint8_t GameTracker::readMemory(uint16_t address)
+{
+	return memory[address];
+}
+
+void GameTracker::setMemoryBase(uint8_t *base)
+{
+	memory = base;
+}
